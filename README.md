b64184dd28ee64b84b15befcfb2fc9d88696e3c3
Muhammad Abdullah <its.abd612@gmail.com>
2021-02-24 18:25:06 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Power Electronics Lab - Spring 2018
+
+Various lab tasks and assignments for Power Electronics Lab
+
+## Description
+
+* Language: C
+
+## Developer
+ 
+[@abd612](https://github.com/abd612)
---
+++ midterm-project/control_firing_angle/MDK-ARM/RTE/_control_firing_angle/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'control_firing_angle' 
+ * Target:  'control_firing_angle' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
